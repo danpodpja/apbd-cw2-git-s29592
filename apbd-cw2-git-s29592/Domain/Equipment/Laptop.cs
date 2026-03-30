f6a432bb55da7b89af1360cda86719718ce5fac2
Daniel Podstawka <s29592@pjwstk.edu.pl>
2026-03-30 20:14:41 +0200

    implement Equipment types

---
+++ apbd-cw2-git-s29592/Domain/Equipment/Laptop.cs
@@ -0,0 +1,18 @@
+﻿namespace apbd_cw2_git_s29592.Domain.Equipment;
+
+public class Laptop : Equipment
+{
+    public int RamGb      { get; set; }
+    public int StorageGb  { get; set; }
+
+    public Laptop(string name, int ramGb, int storageGb) : base(name)
+    {
+        RamGb     = ramGb;
+        StorageGb = storageGb;
+    }
+
+    public override string GetTypeName() => "Laptop";
+
+    public override string ToString() =>
+        $"{base.ToString()} | RAM: {RamGb} GB, SSD: {StorageGb} GB";
+}
