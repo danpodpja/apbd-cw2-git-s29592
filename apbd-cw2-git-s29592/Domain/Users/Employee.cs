57cc2d520d251a7585e5f452271052a9dd83c449
Daniel Podstawka <s29592@pjwstk.edu.pl>
2026-03-30 20:29:24 +0200

    add User and its types

---
+++ apbd-cw2-git-s29592/Domain/Users/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace apbd_cw2_git_s29592.Domain.Users;
+
+public class Employee : User
+{
+    public string Department { get; set; }
+    public string EmployeeNumber { get; set; }
+
+    public Employee(string firstName, string lastName, string department, string employeeNumber)
+        : base(firstName, lastName)
+    {
+        Department = department;
+        EmployeeNumber = employeeNumber;
+    }
+
+    public override string GetUserType() => "Employee";
+
+    public override string ToString() =>
+        $"{base.ToString()} | Wydział: {Department}, Nr: {EmployeeNumber}";
+}
