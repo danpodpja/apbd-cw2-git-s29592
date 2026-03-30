dea923797b8cff3fa315b3c3f7ef05386960937c
Daniel Podstawka <s29592@pjwstk.edu.pl>
2026-03-30 20:07:45 +0200

    add abstract Equipment

---
+++ apbd-cw2-git-s29592/Domain/Equipment/Equipment.cs
@@ -0,0 +1,24 @@
+﻿namespace apbd_cw2_git_s29592.Domain.Equipment;
+
+public abstract class Equipment
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public string Name { get; set; }
+    public EquipmentStatus Status { get; private set; } = EquipmentStatus.Available;
+
+    protected Equipment(string name)
+    {
+        Name = name;
+    }
+
+    public bool IsAvailable => Status == EquipmentStatus.Available;
+
+    public void MarkAsRented()      => Status = EquipmentStatus.Rented;
+    public void MarkAsAvailable()   => Status = EquipmentStatus.Available;
+    public void MarkAsUnavailable() => Status = EquipmentStatus.Unavailable;
+
+    public abstract string GetTypeName();
+
+    public override string ToString() =>
+        $"[{Id.ToString()[..8]}] {GetTypeName()}: {Name} ({Status})";
+}
